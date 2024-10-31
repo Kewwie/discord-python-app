8a3d363c50a78ea420055319e23c9ea0389c7f5d
Kevin Lidström <iamkewwie@gmail.com>
2024-10-31 01:27:30 +0100

    updated so works yes yes

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -6,7 +6,19 @@ import json
 def config():
     with open("config.json") as file:
         return json.load(file)
+    
+async def getUserId(ctx: commands.Context) -> int:
+    user = ctx.message.content.split(" ")[1]
 
+    if user.isdigit():
+        return int(user)
+    elif (user == "u") and ctx.message.reference: 
+        message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+        return int(message.author.id)
+    elif user.startswith("<@") and user.endswith(">"):
+        return int(user[3:-1])
+    else:
+        return None
 
 class Moderation(commands.Cog, name="moderation"):
     def __init__(self, client) -> None:
@@ -23,19 +35,20 @@ class Moderation(commands.Cog, name="moderation"):
         return commands.check(predicate)
     
     def is_self():
-        def predicate(ctx: commands.Context):
-            if ctx.author.id != ctx.message.mentions[0].id:
+        async def predicate(ctx: commands.Context):
+
+            if ctx.author.id != await getUserId(ctx):
                 return True
             raise commands.CheckFailure("You can't perform this action on yourself")
         return commands.check(predicate)
     
     def is_higher():
-        def predicate(ctx: commands.Context):
+        async def predicate(ctx: commands.Context):
             author_roles = [role.position for role in ctx.author.roles]
-            mentioned_roles = [role.position for role in ctx.message.mentions[0].roles]
+            user_roles = [role.position for role in ctx.guild.get_member(await getUserId(ctx)).roles]
 
             highest_author_role = max(author_roles)
-            highest_mentioned_role = max(mentioned_roles)
+            highest_mentioned_role = max(user_roles)
 
             if highest_author_role > highest_mentioned_role:
                 return True
@@ -51,7 +64,11 @@ class Moderation(commands.Cog, name="moderation"):
     @is_staff()
     @is_self()
     @is_higher()
-    async def timeout(self, ctx: commands.Context, member: discord.Member, time: str, *, reason: str = "No reason provided") -> None:
+    async def timeout(self, ctx: commands.Context, member: str, time: str, *, reason: str = "No reason provided") -> None:
+        member = ctx.guild.get_member(await getUserId(ctx))
+        if member == ctx.author:
+            await ctx.send("You can't timeout yourself", delete_after=3)
+
         if time.endswith("s"):
             time = datetime.timedelta(seconds=int(time[:-1]))
         elif time.endswith("m"):
@@ -102,8 +119,9 @@ class Moderation(commands.Cog, name="moderation"):
         aliases=["infraction", "infs"]
     )
     @commands.bot_has_permissions(send_messages=True)
-    async def infractions(self, ctx: commands.Context, member: discord.Member = None) -> None:
-        if not member: member = ctx.author
+    async def infractions(self, ctx: commands.Context, member: str = None) -> None:
+        if not member: member = ctx.author.id
+        member = ctx.guild.get_member(await getUserId(ctx))
   
         infractions = self.client.database.infractions.find({
             "guild_id": ctx.guild.id,
@@ -135,6 +153,38 @@ class Moderation(commands.Cog, name="moderation"):
         await ctx.send(embed=embed)
 
     @commands.command(
+        name="clear-infraction",
+        description="Clear an infraction from a user",
+        aliases=["clearinf", "del-infraction", "delinf"]
+    )
+    @is_staff()
+    @is_self()
+    async def clear_infraction(self, ctx: commands.Context, member: str, infraction_number: int) -> None:
+        member = ctx.guild.get_member(await getUserId(ctx))
+        infractions = self.client.database.infractions.find({
+            "guild_id": ctx.guild.id,
+            "user_id": member.id
+        })
+
+        if self.client.database.infractions.count_documents({
+            "guild_id": ctx.guild.id,
+            "user_id": member.id
+        }) == 0:
+            await ctx.send(f"**{member.name}** has no infractions")
+            return
+
+        infraction_number = 0
+        for infraction in infractions:
+            infraction_number += 1
+            if infraction_number == infraction_number:
+                self.client.database.infractions.delete_one({
+                    "guild_id": ctx.guild.id,
+                    "user_id": member.id,
+                    "reason": infraction["reason"]
+                })
+                await ctx.send(f"Infraction {infraction_number} has been cleared")
+
+    @commands.command(
         name="ban",
         description="Ban a user from the server"
     )
@@ -142,7 +192,11 @@ class Moderation(commands.Cog, name="moderation"):
     @is_staff()
     @is_self()
     @is_higher()
-    async def ban(self, ctx: commands.Context, user: discord.User, *, reason: str = "No reason provided") -> None:
+    async def ban(self, ctx: commands.Context, user: str, *, reason: str = "No reason provided") -> None:
+        user = ctx.guild.get_member(await getUserId(ctx))
+        if user == ctx.author:
+            await ctx.send("You can't ban yourself", delete_after=3)
+
         await ctx.message.delete()
 
         ban_entry = await ctx.guild.fetch_ban(user)
